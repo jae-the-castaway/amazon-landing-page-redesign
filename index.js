3db8d639ab2ffdb9fc49cee49b48787612c66321
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-07-03 19:02:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Create a new paragraph element
+const paragraph = document.createElement("p");
+
+// Set the text content of the paragraph
+paragraph.textContent = "Hello, World!";
+
+// Change the color of the paragraph
+paragraph.style.color = "red";
+
+// Append the paragraph to the document body or any other desired element
+document.body.appendChild(paragraph);
